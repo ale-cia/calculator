3d17ce35535e2f8e77756b41b4a887ddd18948b0
ale-cia <ale-cia@users.noreply.github.com>
2017-07-06 11:55:09 -0500

    Update client.js

--- server/public/views/client.js
+++ server/public/views/client.js
@@ -2,20 +2,31 @@ $(document).ready(function(){
 		var numOne = parseInt($("#numOne").val(), 10);
         var numTwo = parseInt($("#numTwo").val(), 10);
 
-      $("#add").click(function() {
+      $("button").click(function(e) {
       		var numOne = parseInt($("#numOne").val(), 10);
         	var numTwo = parseInt($("#numTwo").val(), 10);
-        	var addition = numOne + numTwo;
+            var idClicked= e.target.id; 
+        
+        var operation= function() {	
+        	if(idClicked === "add") {        	
+        		var addition = numOne + numTwo;
+        		return addition;
+  				}
 
+  			else if(idClicked === "subtract") {        	
+        		var subtract = numOne - numTwo;
+        		return subtract;
+  				}		
+        }
 
-         	$("#submit").on("click", function(){
-                console.log(addition);
+         	$("#submit").on("click", function(){ 
+                console.log(operation());
+        })
+  	});
 
-        });
-  	})
       $("#clear").on("click", function(){
       		$("#numOne").val(' ');
         	$("#numTwo").val(' ');
- 	})
 
-   });
+ 	})
+ });
